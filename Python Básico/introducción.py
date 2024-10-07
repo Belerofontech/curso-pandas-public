e4cee222e80009e9e0520fc8fccab9ea9266e0e2
Benito Cuezva Rubio <benito.cuezva@belerofontech.com>
2024-10-07 21:29:14 +0200

    Creación de ficheros para el arranque del curso.

---
+++ Python Básico/introducción.py
@@ -0,0 +1,33 @@
+# Variables
+
+a = 10
+b = 2
+
+print( b / a )
+
+# Cadenas de texto
+cadena ="supercalifragilistico"
+stringy = "patata"
+
+print(cadena + " " + stringy)
+# Listas
+
+# Diccionarios
+
+# Condicionales
+
+
+
+# Bucles
+
+
+# Comprehensións
+
+# Funciones
+
+
+# Numpy
+
+# Pandas
+
+
